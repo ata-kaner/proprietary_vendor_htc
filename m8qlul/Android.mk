987924a26eac1be1c0640ce84e04436b4c5f6d6a
Olricccc <ata_kaner@outlook.com>
2023-05-10 20:19:22 +0300

    Get rid of adreno lib symlinks
    We are symlinking them with device tree from now on.

--- m8qlul/Android.mk
+++ m8qlul/Android.mk
@@ -82,9 +82,6 @@ LOCAL_MODULE_PATH_32 := $(2ND_TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-
 endif
 endif
 
